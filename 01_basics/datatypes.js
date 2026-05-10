675b84bd5ada6860d4008c4c687ddbf6ecdb3586
Kamaljeetmishra69 <kamaljeetmishra69@gmail.com>
2026-05-10 09:03:57 +0000

    datatypes and variables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as newer vrsion 
+
+//alert(3+4); // it support in browser only not in node enviornment it has different syntax in node
+let name = "kamaljeet"
+let age = 25
+let isLoggedIn = false 
+
+
+
+
+// number => 2 to power 53
+// bigint
+//string=>""
+//boolean => true,false
+//null =>standaloe value
+//undefined =>
+    //symbol =>unique
+
+
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);// object
---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 34562
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "delhi"
+let accountState
+
+// accountId = 2 // not  allowed 
+
+accountEmail = "abcde@gmail.com"
+accountPassword ="2121212"
+accountCity = "bengluru"
+
+console.log(accountId);
+
+console.table([ accountId, accountEmail, accountPassword, accountCity,accountState])
