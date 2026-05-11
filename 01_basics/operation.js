136feacf44f468171edb511e765698880ed75b46
Kamaljeetmishra69 <kamaljeetmishra69@gmail.com>
2026-05-11 07:51:44 +0000

    operations in js

---
+++ 01_basics/operation.js
@@ -0,0 +1,59 @@
+// let value = 3
+// let negvalue = -value;
+// console.log(negvalue);
+
+
+// console.log(4+4)
+// console.log(4-4)
+// console.log(4*4)
+// console.log(2**4)
+// console.log(2/3);
+// console.log(2%3);
+
+// // string addition 
+
+// let str1 = "hello"
+// let str2 = " kamaljeet"
+
+// let str3 = str1 + str2;
+// console.log(str3)
+
+// console.log("1" + 3);
+// console.log(1 +"3")
+// console.log(1+1+"3");
+// console.log("1"+1+3);
+
+// console.log(true)
+// console.log(+true)
+// // console.log(true+)
+// console.log("")
+// console.log(+"")
+
+
+// let x=4;
+
+// console.log(x++);
+
+
+//prefix & postfix
+// let x = 3;
+// const y = x++;
+// console.log(`x:${x}, y:${y}`)
+
+// let a =2;
+// const b= ++a;
+// console.log(`a:${a}, b:${b}`)
+
+
+
+//avoid these  types ofcomparsion
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null > 0);
+// console.log(null == 0)
+// console.log(null >=0)
+
+
+// === strict check comparision iy check datatypes also 
+
