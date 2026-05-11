d2937309de27fdaaf1f72adfedc0ded9ce3900bd
Kamaljeetmishra69 <kamaljeetmishra69@gmail.com>
2026-05-11 08:49:56 +0000

    datatypes summary

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,30 @@
+//*****************************datatypes summary****************************
+
+// primtive datatypes & reference dataypes(non premitive)
+
+// primitive dataypes 
+//string ,number,boolean , symbol, BigInt, null,undefined
+
+
+//reference types
+//Array, function, objects
+
+let heros =["shaktiman ","akchay khana","me" ]
+console.log(heros)
+console.log(typeof heros)
+
+
+ let myObject={
+    name :"kamaljeet Mishra",
+    course : "MCA",
+    enrollmentId  : "A453145024001"
+}
+console.log(myObject)
+console.log(typeof myObject)
+
+const myFunction function(){
+    console.log("hello world")
+}
+console.log(myFunction)
+console,log(typeof myFunction);
+
