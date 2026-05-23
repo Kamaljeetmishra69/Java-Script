1a892b24aeceaf2247a01196777e5a0d021dde85
Kamaljeetmishra69 <kamaljeetmishra69@gmail.com>
2026-05-23 11:10:57 +0000

    ok

---
+++ 05_Iteration/four.js
@@ -0,0 +1,12 @@
+const arrayObject = ["js","ruby","cpp","java","c#"]
+
+arrayObject.forEach(  function (i) {
+    console.log(i);
+})
+
+
+// for each loop arrow function implimentation
+
+arrayObject.forEach((item)=>{
+    console.log(item)
+})
