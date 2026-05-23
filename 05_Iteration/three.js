ff96ba19d3fe2da0bcae323eca3f0e8c285bf6bb
Kamaljeetmishra69 <kamaljeetmishra69@gmail.com>
2026-05-23 10:37:28 +0000

    updated

--- 05_Iteration/three.js
+++ 05_Iteration/three.js
@@ -5,7 +5,7 @@
 
  const  arr = [1,2,3,4,5,6,7,8,9,0]
 for (const num of arr) {
-    console.log(num)
+   // console.log(num)
 }
 
 
@@ -13,8 +13,18 @@ for (const num of arr) {
 //map 
 
 const map = new Map()
-map.set =('in',india)
+map.set =('in','india')
 map.set =('usa','united state of america')
 map.set =('uae ','united arab emirates')
 
-console.log(map)
+//console.log(map)
+
+// for of loop map and array pr kaam to krt hai lekin thoda sa alg kaam krt ahai 
+ for (const [key, value] of map) {
+    console.log(key, value)     // yaha code chl nahi rha kuch problem hai 
+
+
+ }
+
+
+ // for of loop object pr kaam nahi karta isliye for in loop use hota hai ham dekhenge ki agge file me 
