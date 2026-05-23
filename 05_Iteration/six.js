c9ed2a1afb2e0a62c717ae77c7262ca2e0d4f6f8
Kamaljeetmishra69 <kamaljeetmishra69@gmail.com>
2026-05-23 13:26:00 +0000

    okdone

---
+++ 05_Iteration/six.js
@@ -0,0 +1,13 @@
+// map function is same use as filter method is used, map also return value 
+//lets see example of map how does it work 
+const arrItem = [1,2,3,4,5,6,7,8,9,10];
+const sortedValue = arrItem.map((i) => {return i + 10})     
+console.log (sortedValue)
+ 
+
+
+
+//chaining in javascript 
+// when two or more function is used in continuous way then it is said to be chaining 
+// let us see through  a example  
+
