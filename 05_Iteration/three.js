a345caa4987a06cbdf31034a8c00c1ee010b8e8f
Kamaljeetmishra69 <kamaljeetmishra69@gmail.com>
2026-05-23 10:23:33 +0000

    ok

---
+++ 05_Iteration/three.js
@@ -0,0 +1,20 @@
+//foe of loop 
+
+// ["","",""]
+// [{},{},{}]
+
+ const  arr = [1,2,3,4,5,6,7,8,9,0]
+for (const num of arr) {
+    console.log(num)
+}
+
+
+
+//map 
+
+const map = new Map()
+map.set =('in',india)
+map.set =('usa','united state of america')
+map.set =('uae ','united arab emirates')
+
+console.log(map)
